e30303b37edd3711cb4e7cb73f17cd151b581479
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2025-06-22 11:11:27 +0200

    fixed record wrapper

--- crisp_gym/lerobot_wrapper.py
+++ crisp_gym/lerobot_wrapper.py
@@ -1,41 +1,46 @@
 """Wrapper to convert the outputs of the step function to the proper LeRobotDataset expected format."""
 
 from typing import Dict
+
+from lerobot.common.datasets.lerobot_dataset import CODEBASE_VERSION
+
 from crisp_gym.manipulator_env import ManipulatorBaseEnv
-from lerobot.common.datasets.lerobot_dataset import VERSION
 
 
-def get_features(env: ManipulatorBaseEnv) -> Dict[str, str]:
+def get_features(env: ManipulatorBaseEnv) -> Dict[str, Dict]:
     """Get the features used by LeRobotDataset."""
-    if VERSION != "2.0":
-        pass  # TODO: add warning
+    if not CODEBASE_VERSION.startswith("v2"):
+        print(
+            "WARNING: this function has been implemented for version 2.x of LeRobotDataset. Expect unexpected behaviour for other versions."
+        )
 
     features = {}
     for cam in env.cameras:
         # TODO: cmaera feature
         # camera_key_map = {"third_person_image": "primary", "wrist_image": "wrist"}
-        features[f"observation.images.{cam.name}"] = {
+        features[f"observation.images.{cam.config.camera_name}"] = {
             "dtype": "float32",
-            "shape": (36, 36),
+            "shape": cam.config.resolution,
         }
 
     # joint_dims = [f"joint_{idx}" for idx in range(7)] + ["gripper"]
-    # cartesian_dims = ["x", "y", "z", "roll", "pitch", "yaw", "gripper"]
+    cartesian_dims = ["x", "y", "z", "roll", "pitch", "yaw", "gripper"]
 
     # Propioceptive
     # Feature configuration
     features["observation.state"] = {
         "dtype": "float32",
-        "shape": (len(env.observation_space),),
-        "names": env.observation_space.names,
+        "shape": (len(cartesian_dims),),
+        "names": cartesian_dims,  # TODO: deal with other types
     }
 
     # Action
     features["action"] = {
         "dtype": "float32",
-        "shape": (len(env.action_space),),
-        "names": env.action_space.names,
+        "shape": env.action_space.shape,
+        "names": cartesian_dims,  # TODO: deal with other types
     }
 
     # TODO: add further observations
+
     return features
