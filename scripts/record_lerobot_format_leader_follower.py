fc6c08b1a67a5e01cda1d091f0708b85d8b95dc0
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2025-07-03 15:11:47 +0200

    Added some flags and forced users to have a crisp config environment variable

--- scripts/record_lerobot_format_leader_follower.py
+++ scripts/record_lerobot_format_leader_follower.py
@@ -1,6 +1,7 @@
 """Script showcasing how to record data in Lerobot Format."""
 
 import argparse  # noqa: I001
+import os
 import shutil
 import time
 from pathlib import Path
@@ -10,7 +11,6 @@ from crisp_py.gripper import Gripper, GripperConfig
 from crisp_py.robot import Robot
 import numpy as np
 
-from crisp_py.camera import FrankaCameraConfig
 from lerobot.common.datasets.lerobot_dataset import HF_LEROBOT_HOME, LeRobotDataset
 from lerobot.common.datasets.utils import build_dataset_frame
 from rich import print
@@ -18,106 +18,151 @@ from rich import print
 import crisp_gym  # noqa: F401
 from crisp_gym.lerobot_wrapper import get_features
 from crisp_gym.manipulator_env import ManipulatorCartesianEnv
-from crisp_gym.manipulator_env_config import FrankaEnvConfig
+from crisp_gym.manipulator_env_config import AlohaFrankaEnvConfig
 from crisp_gym.record.recording_manager import RecordingManager
 
 
 parser = argparse.ArgumentParser(description="Record data in Lerobot Format")
 parser.add_argument(
-    "--repo-id", type=str, default="franka_single", help="Repository ID for the dataset"
+    "--repo-id",
+    type=str,
+    default="franka_single",
+    help="Repository ID for the dataset",
 )
-parser.add_argument("--task", type=str, default="pick the lego block.", help="Task description")
-parser.add_argument("--robot-type", type=str, default="franka", help="Type of robot being used")
-parser.add_argument("--fps", type=int, default=30, help="Frames per second for recording")
 parser.add_argument(
-    "--max-duration", type=float, default=30.0, help="Maximum episode duration in seconds"
+    "--task",
+    type=str,
+    default="pick the lego block.",
+    help="Task description",
 )
-parser.add_argument("--num-episodes", type=int, default=1, help="Number of episodes to record")
-parser.add_argument("--resume", type=bool, default=False, help="Resume recording of an already existing dataset")
-
+parser.add_argument(
+    "--robot-type",
+    type=str,
+    default="franka",
+    help="Type of robot being used",
+)
+parser.add_argument(
+    "--fps",
+    type=int,
+    default=30,
+    help="Frames per second for recording",
+)
+parser.add_argument(
+    "--max-duration",
+    type=float,
+    default=30.0,
+    help="Maximum episode duration in seconds",
+)
+parser.add_argument(
+    "--num-episodes",
+    type=int,
+    default=1,
+    help="Number of episodes to record",
+)
+parser.add_argument(
+    "--resume",
+    type=bool,
+    default=False,
+    help="Resume recording of an already existing dataset",
+)
+parser.add_argument(
+    "--push-to-hub",
+    type=bool,
+    default=True,
+    help="Whether to push the dataset to the Hugging Face Hub.",
+)
+parser.add_argument(
+    "--leader-controller",
+    type=str,
+    default="gravity_compensation_on_plane",
+    help="Controller configuration for the leader robot.",
+)
+parser.add_argument(
+    "--right-gripper-config",
+    type=str,
+    default="gripper_right",
+    help="Gripper configuration for the right robot.",
+)
+parser.add_argument(
+    "--trigger-config",
+    type=str,
+    default="trigger",
+    help="Trigger configuration for the leader robot.",
+)
+# TODO: @maxdoesch add this
+parser.add_argument(
+    "--joint-control",
+    type=bool,
+    default=False,
+    help="Whether to use joint control for the robot.",
+)
 
 args = parser.parse_args()
 
-repo_id = args.repo_id
-single_task = args.task
-robot_type = args.robot_type
-fps = args.fps
-max_episode_duration = args.max_duration
-num_episodes = args.num_episodes
-resume = args.resume
 
 # Clean up existing dataset if it exists
-if Path(HF_LEROBOT_HOME / repo_id).exists():
-    shutil.rmtree(HF_LEROBOT_HOME / repo_id)
+if Path(HF_LEROBOT_HOME / args.repo_id).exists():
+    shutil.rmtree(HF_LEROBOT_HOME / args.repo_id)
 
-# Wrist camera
-camera_config_wrist = FrankaCameraConfig()
-camera_config_wrist.camera_name = "right_wrist_camera"
-camera_config_wrist.camera_color_image_topic = "right_wrist_camera/color/image_rect_raw"
-camera_config_wrist.camera_color_info_topic = "right_wrist_camera/color/camera_info"
-
-# Tp camera
-camera_config_tp = FrankaCameraConfig()
-camera_config_tp.camera_name = "right_third_person_camera"
-camera_config_tp.camera_color_image_topic = "right_third_person_camera/color/image_raw"
-camera_config_tp.camera_color_info_topic = "right_third_person_camera/color/camera_info"
-
-env_config = FrankaEnvConfig(camera_configs=[camera_config_wrist, camera_config_tp])
-env_config.gripper_enabled = True
-
-path = Path("../crisp_py/config/gripper_right.yaml").resolve()
-env_config.gripper_config = GripperConfig.from_yaml(path=path)
-env_config.gripper_config.joint_state_topic = "gripper/gripper_state_broadcaster/joint_states"
-env_config.gripper_config.command_topic = "gripper/gripper_position_controller/commands"
-env_config.gripper_continous_control = True
-
-env = ManipulatorCartesianEnv(namespace="right", config=env_config)
-features = get_features(env)
-
-if resume:
-    print(f"[green]Resuming recording from existing dataset: {repo_id}")
-    dataset = LeRobotDataset(repo_id=repo_id)
-else:
-    print(f"[green]Creating new dataset: {repo_id}")
-    dataset = LeRobotDataset.create(
-        repo_id=repo_id,
-        fps=fps,
-        robot_type=robot_type,
-        features=features,
-        use_videos=False,
+# Set up the config for the environment
+path_to_config = os.environ.get("CRISP_CONFIG_PATH")
+if path_to_config is None:
+    raise ValueError(
+        "You need to set the environment variable CRISP_CONFIG_PATH in order to load configs for the gripper and controller.\nTo do this execute export CRISP_CONFIG_PATH=path\\to\\config."
     )
 
-# Leader
+# Set up the envionment configuration
+gripper_config = GripperConfig.from_yaml(
+    path=(Path(path_to_config) / args.right_griper_config + ".yaml").resolve()
+)
+gripper_config.joint_state_topic = "gripper" + gripper_config.joint_state_topic
+gripper_config.command_topic = "gripper" + gripper_config.command_topic
+
+env_config = AlohaFrankaEnvConfig(gripper_config=gripper_config)
+env = ManipulatorCartesianEnv(namespace="right", config=env_config)
+
+# %% Prepare the Leader
 leader = Robot(namespace="left")
 leader.wait_until_ready()
 
-path = Path("../crisp_py/config/trigger.yaml").resolve()
+leader_gripper_path = (Path(path_to_config) / args.trigger_config).resolve()
 leader_gripper = Gripper(
-    gripper_config=GripperConfig.from_yaml(path=path),
+    gripper_config=GripperConfig.from_yaml(path=leader_gripper_path),
     namespace="left/gripper",
-    index=1,
 )
 leader_gripper.wait_until_ready()
 leader_gripper.disable_torque()
 
-# %% Prepare environment and leader
+# %% Prepare the dataset
+features = get_features(env)
 
+if args.resume:
+    print(f"[green]Resuming recording from existing dataset: {args.repo_id}")
+    dataset = LeRobotDataset(repo_id=args.repo_id)
+else:
+    print(f"[green]Creating new dataset: {args.repo_id}")
+    dataset = LeRobotDataset.create(
+        repo_id=args.repo_id,
+        fps=args.fps,
+        robot_type=args.robot_type,
+        features=features,
+        use_videos=False,
+    )
+
+# %% Prepare environment and leader
 env.home()
 env.reset()
-
 env.robot.controller_switcher_client.switch_controller("cartesian_impedance_controller")
 
-leader.wait_until_ready()
 leader.cartesian_controller_parameters_client.load_param_config(
-    file_path="../crisp_py/config/control/gravity_compensation_on_plane.yaml"
+    file_path=Path(path_to_config) / "control" / args.leader_controller + ".yaml"
 )
 leader.home()
 leader.controller_switcher_client.switch_controller("cartesian_impedance_controller")
 
 # %% Start interaction
-
 previous_pose = leader.end_effector_pose
-with RecordingManager(num_episodes=num_episodes) as recording_manager:
+with RecordingManager(num_episodes=args.num_episodes) as recording_manager:
     while not recording_manager == "exit":
         print(
             f"[magenta bold]=== Episode {recording_manager.episode_count + 1} / {recording_manager.num_episodes} ==="
@@ -131,6 +176,7 @@ with RecordingManager(num_episodes=num_episodes) as recording_manager:
         print("[blue]Started episode")
 
         start_time = time.time()
+        # TODO: @danielsanjosepro remove the step variable and use a flag to indicate the first step
         step = 0
         obs = {}
 
@@ -141,7 +187,7 @@ with RecordingManager(num_episodes=num_episodes) as recording_manager:
         env.robot.reset_targets()
         leader.reset_targets()
         leader.cartesian_controller_parameters_client.load_param_config(
-            file_path="../crisp_py/config/control/gravity_compensation_on_plane.yaml"
+            file_path=Path(path_to_config) / "control" / args.leader_controller + ".yaml"
         )
         leader.controller_switcher_client.switch_controller("cartesian_impedance_controller")
         env.robot.controller_switcher_client.switch_controller("cartesian_impedance_controller")
@@ -151,6 +197,7 @@ with RecordingManager(num_episodes=num_episodes) as recording_manager:
 
             if step == 0:
                 previous_pose = leader.end_effector_pose
+                # TODO: @danielsanjosepro make this steps clearer to the user
                 obs, _, _, _, _ = env.step(
                     action=np.concatenate(
                         [
@@ -178,6 +225,7 @@ with RecordingManager(num_episodes=num_episodes) as recording_manager:
             # print("Actual time is: ", time.time() - start_time)
             # print("Step time is: ", env.timestep * 1 / 30)
 
+            # TODO: @danielsanjosepro make this steps clearer to the user
             action_pose = leader.end_effector_pose - previous_pose
             previous_pose = leader.end_effector_pose
 
@@ -197,6 +245,7 @@ with RecordingManager(num_episodes=num_episodes) as recording_manager:
                 ]
             )
 
+            # TODO: @danielsanjosepro or @maxdoesch make this saving in dict format more elegant
             action_dict = {dim: action[i] for i, dim in enumerate(features["action"]["names"])}
             obs_dict = {
                 dim: obs["cartesian"][i] if i < 6 else obs["gripper"][0]
@@ -212,14 +261,13 @@ with RecordingManager(num_episodes=num_episodes) as recording_manager:
             obs_frame = build_dataset_frame(features, obs_dict, prefix="observation.state")
 
             frame = {**obs_frame, **action_frame, **cam_frame}
-            dataset.add_frame(frame, task=single_task)
+            dataset.add_frame(frame, task=args.single_task)
 
             obs, _, _, _, _ = env.step(action, block=False)
 
-            sleep_time = 1 / 30.0 - (time.time() - step_time_init)
+            sleep_time = 1 / args.fps - (time.time() - step_time_init)
             if sleep_time > 0:
                 time.sleep(sleep_time)  # Sleep to allow the environment to process the action
-            step += 1
 
             step += 1
 
@@ -248,7 +296,9 @@ with RecordingManager(num_episodes=num_episodes) as recording_manager:
         if recording_manager.state == "exit":
             break
 
-dataset.push_to_hub(repo_id=repo_id, private=True)
+if args.push_to_hub:
+    print(f"[green]Pushing dataset to Hugging Face Hub with repo_id: {args.repo_id}")
+    dataset.push_to_hub(repo_id=args.repo_id, private=True)
 
 leader.home()
 leader.shutdown()
