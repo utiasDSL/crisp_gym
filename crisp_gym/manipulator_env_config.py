929f3158b0a0a12194ab8a1b24ad2ddf0f368bbb
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2025-08-10 13:43:22 +0200

    feat: better configs for environment

--- crisp_gym/manipulator_env_config.py
+++ crisp_gym/manipulator_env_config.py
@@ -8,23 +8,59 @@ from typing import List
 from crisp_py.camera.camera_config import CameraConfig
 from crisp_py.gripper.gripper import GripperConfig
 from crisp_py.robot_config import FrankaConfig, RobotConfig
+from crisp_py.sensors.sensor_config import SensorConfig
 
 from crisp_gym.config.path import CRISP_CONFIG_PATH
 
 
-@dataclass
+@dataclass(kw_only=True)
 class ManipulatorEnvConfig(ABC):
-    """Manipulator Gym Environment Configuration."""
+    """Manipulator Gym Environment Configuration.
+
+    This class serves as a base configuration for manipulator environments.
+    It includes parameters for control frequency, robot configuration,
+    gripper configuration, camera configurations, and control parameters.
+
+    Attributes:
+        control_frequency (float): The frequency at which the environment is controlled.
+        robot_config (RobotConfig): Configuration for the robot.
+        gripper_config (GripperConfig | None): Configuration for the gripper, if applicable.
+        camera_configs (List[CameraConfig]): List of camera configurations.
+        cartesian_control_param_config (Path | None): Path to the Cartesian control parameters configuration file.
+        joint_control_param_config (Path | None): Path to the joint control parameters configuration file.
+        gripper_threshold (float): Threshold for gripper actions.
+        gripper_enabled (bool): Whether the gripper is enabled.
+        gripper_continuous_control (bool): Whether the gripper supports continuous control.
+        max_episode_steps (int | None): Maximum number of steps per episode, if applicable.
+    """
 
     control_frequency: float
     robot_config: RobotConfig
     gripper_config: GripperConfig | None
     camera_configs: List[CameraConfig]
 
+    cartesian_control_param_config: Path | None
+    joint_control_param_config: Path | None
+
+    sensor_configs: List[SensorConfig] = field(default_factory=lambda: [])
+
     gripper_threshold: float = 0.1
     gripper_enabled: bool = True
     gripper_continuous_control: bool = False
 
+    max_episode_steps: int | None = None
+
+
+# === Franka Robotics FR3 Environment Configurations ===
+@dataclass
+class FrankaEnvConfig(ManipulatorEnvConfig, ABC):
+    """Franka Gym Environment Configuration."""
+
+    control_frequency: float = 30.0
+
+    robot_config: RobotConfig = field(default_factory=lambda: FrankaConfig())
+
+    # Default controller configurations for Franka
     cartesian_control_param_config: Path | None = field(
         default_factory=lambda: CRISP_CONFIG_PATH / "control" / "default_cartesian_impedance.yaml"
     )
@@ -32,71 +68,19 @@ class ManipulatorEnvConfig(ABC):
         default_factory=lambda: CRISP_CONFIG_PATH / "control" / "joint_control.yaml"
     )
 
-    max_episode_steps: int | None = None
-
 
 @dataclass
-class FrankaEnvConfig(ManipulatorEnvConfig):
+class NoCamFrankaEnvConfig(FrankaEnvConfig):
     """Franka Gym Environment Configuration."""
 
-    control_frequency: float = 10.0
-
-    gripper_threshold: float = 0.1
-
-    robot_config: RobotConfig = field(default_factory=lambda: FrankaConfig())
-
-    gripper_config: GripperConfig | None = field(
-        default_factory=lambda: GripperConfig(
-            min_value=0,
-            max_value=1,
-            command_topic="gripper/gripper_position_controller/commands",
-            joint_state_topic="gripper/joint_states",
-            reboot_service="gripper/reboot_gripper",
-            enable_torque_service="gripper/dynamixel_hardware_interface/set_dxl_torque",
-        )
-    )
-
-    camera_configs: List[CameraConfig] = field(
-        default_factory=lambda: [
-            CameraConfig(
-                camera_name="primary",
-                camera_frame="primary_link",
-                resolution=(256, 256),
-                camera_color_image_topic="right_third_person_camera/color/image_raw",
-                camera_color_info_topic="right_third_person_camera/color/camera_info",
-            ),
-            CameraConfig(
-                camera_name="wrist",
-                camera_frame="wrist_link",
-                resolution=(256, 256),
-                camera_color_image_topic="right_wrist_camera/color/image_rect_raw",
-                camera_color_info_topic="right_wrist_camera/color/camera_info",
-            ),
-        ]
-    )
-
-    max_episode_steps: int | None = 1000
-
-
-@dataclass
-class NoCamFrankaEnvConfig(ManipulatorEnvConfig):
-    """Franka Gym Environment Configuration."""
-
-    control_frequency: float = 10.0
-
-    gripper_threshold: float = 0.1
     gripper_continuous_control: bool = True
     gripper_enabled: bool = False
-
-    robot_config: RobotConfig = field(default_factory=lambda: FrankaConfig())
 
     gripper_config: GripperConfig | None = field(
         default_factory=lambda: GripperConfig(min_value=0, max_value=1)
     )
 
     camera_configs: List[CameraConfig] = field(default_factory=lambda: [])
-
-    max_episode_steps: int | None = 1000
 
 
 @dataclass
@@ -126,15 +110,8 @@ class RightNoCamFrankaEnvConfig(NoCamFrankaEnvConfig):
 
 
 @dataclass
-class OnlyWristCamFrankaEnvConfig(ManipulatorEnvConfig):
+class OnlyWristCamFrankaEnvConfig(FrankaEnvConfig):
     """Franka Gym Environment Configuration."""
-
-    control_frequency: float = 10.0
-
-    gripper_threshold: float = 0.1
-    gripper_enabled: bool = False
-
-    robot_config: RobotConfig = field(default_factory=lambda: FrankaConfig())
 
     gripper_config: GripperConfig | None = field(
         default_factory=lambda: GripperConfig(
@@ -159,20 +136,16 @@ class OnlyWristCamFrankaEnvConfig(ManipulatorEnvConfig):
         ]
     )
 
-    max_episode_steps: int | None = 1000
-
 
 @dataclass
-class AlohaFrankaEnvConfig(ManipulatorEnvConfig):
+class AlohaFrankaEnvConfig(FrankaEnvConfig):
     """Custom Franka Gym Environment Configuration for Franka with an Aloha gripper and cameras."""
 
-    control_frequency: float = 10.0
-
     # The aloha gripper can be controlled in a continuous manner, so we set this to True.
+    # For more information on the gripper, check: https://github.com/TUM-LSY/aloha4franka
     gripper_enabled: bool = True
     gripper_continuous_control: bool = True
 
-    robot_config: RobotConfig = field(default_factory=lambda: FrankaConfig())
     gripper_config: GripperConfig | None = field(
         default_factory=lambda: GripperConfig.from_yaml(
             path=(CRISP_CONFIG_PATH / ("gripper_aloha.yaml")).resolve()
@@ -244,15 +217,9 @@ class RightAlohaFrankaEnvConfig(AlohaFrankaEnvConfig):
 
 
 @dataclass
-class NoCamNoGripperFrankaEnvConfig(ManipulatorEnvConfig):
+class NoCamNoGripperFrankaEnvConfig(FrankaEnvConfig):
     """Franka Gym Environment Configuration without cameras and gripper."""
 
-    control_frequency: float = 10.0
-
-    cartesian_control_param_config: Path | None = None
-    joint_control_param_config: Path | None = None
-
-    robot_config: RobotConfig = field(default_factory=lambda: FrankaConfig())
     gripper_config: GripperConfig | None = field(
         default_factory=lambda: GripperConfig(min_value=0, max_value=1)
     )
