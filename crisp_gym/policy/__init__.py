463ccf57f69c3001193360e5e6388dc4ebcf7b7a
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2025-11-06 16:06:50 +0100

    register async lerobot

--- crisp_gym/policy/__init__.py
+++ crisp_gym/policy/__init__.py
@@ -1,12 +1,12 @@
 """Policy module for crisp_gym."""
 
-# from crisp_gym.policy.async_lerobot_policy import AsyncLerobotPolicy
+from crisp_gym.policy.async_lerobot_policy import AsyncLerobotPolicy
 from crisp_gym.policy.lerobot_policy import LerobotPolicy
 from crisp_gym.policy.policy import Policy, make_policy, register_policy
 
 __all__ = [
     "LerobotPolicy",
-    # "AsyncLerobotPolicy",
+    "AsyncLerobotPolicy",
     "Policy",
     "register_policy",
     "make_policy",
